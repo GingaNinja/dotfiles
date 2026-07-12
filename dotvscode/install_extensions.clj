e58943cb6112067c4683843073ddc5cdbb19b2fd
Dave Arkell <dave@arkells.net>
2026-07-12 17:40:47 +0100

    add extensions for vscode, and setup gpg-agent

---
+++ dotvscode/install_extensions.clj
@@ -0,0 +1,24 @@
+#!/usr/bin/env bb
+
+(require '[babashka.process :refer [shell process exec]])
+
+(let [code_extensions (-> (slurp "./code_extensions.txt")
+                          (str/split-lines)
+                          (set))
+      installed_extensions (->  (shell {:out :string} "code" "--list-extensions")
+                                (:out)
+                                (str/split-lines)
+                                (set))
+      uninstalled_extensions (set/difference code_extensions installed_extensions)]
+  (if (empty? uninstalled_extensions)
+    (println "no extensions to install!")
+    (println "found" (count uninstalled_extensions) "extensions.")
+    ;; (println (first uninstalled_extensions))
+    ;; (println "found" (count uninstalled_extensions) "extensions.")
+    )
+  (println (sort uninstalled_extensions))
+  (dorun (for [ext (sort uninstalled_extensions)]
+           (do (println "Installing " ext "...")
+               (shell "code" "--install-extension" ext)))))
+
+
